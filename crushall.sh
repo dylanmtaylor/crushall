034eaedf2209a842718a4a477fbc3deb59dc490c
Dylan M. Taylor <dylan.m.taylor92@gmail.com>
2015-04-20 18:31:40 -0400

    On CentOS, pngcrush is located at /usr/bin/pngcrush

--- crushall.sh
+++ crushall.sh
@@ -4,7 +4,7 @@
 #Compress all png files, saving the new file with a cmp ending, and then replace the old file.
 #If the file fails to be moved, we probably don't have permission to overwrite the original, so we remove the crush file
 find . -nowarn -name '*.crush' -type f -exec echo "Removing " {} \; -exec rm -f {} \; #Remove old crush files from aborted executions
-export PATH=$PATH:/home/aliendude5300/bin #where pngcrush is located
+export PATH=$PATH:/usr/bin #where pngcrush is located
 find . -nowarn -name '*.png' -type f -exec pngcrush -brute -reduce -fix {} {}.crush \; -exec mv -fv {}.crush {} \;
 find . -nowarn -name '*.crush' -type f -exec echo "Removing " {} \; -exec rm -f {} \; #Cleanup after ourselves...
 echo "PNG Compression Done! :-)"
